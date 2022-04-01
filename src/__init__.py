8bf8d361119b303ff2f91f24cdbc63f4c00b1074
PatrickFranken <patrick.franken@aisler.team>
2022-04-01 11:34:52 +0200

    Fix startup issue on some linux based systems

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,11 @@
 try:
+    # On some Linux based systems site-packages are placed in a different directory.
+    # KiCad only appends the system-wide directory to path,
+    # because of this we add the user-wide directory, too.
+    from sys import path
+    import os
+    path.append(os.path.expanduser('~/.local/lib/python3.9/site-packages'))
+    
     from .plugin import PushForKiCadPlugin
     plugin = PushForKiCadPlugin()
     plugin.register()
